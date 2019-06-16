6c4e26508e28bccf5a2f4e68fff8f28bb3a2e978
NikiDomaPc <nikolaj.grunoski@gmail.com>
2019-06-17 00:42:26 +0200

    Nejasni vezbi

---
+++ index.js
@@ -0,0 +1,75 @@
+// var broj = prompt("Enter a number");
+// var broj = parseInt(broj);
+
+// var posleden = prompt('Enter the ending number to check for even numbers!');
+// var posleden = parseInt(posleden);
+// var result = 0;
+
+// for (var i = broj; i < Number(posleden); i++) { // zasto e Number(posleden) od kade?
+// 	if (i%2===0) {           
+// 		console.log(i);
+// 		result+=0;  // zasto +=0 ?
+// 	}
+// }
+
+// console.log ('Zbirot e ' + result);
+
+
+///
+
+// var names= ["nikolaj","stefi","marija","stole","jovan","sanja","kristina","kocho"];
+// for (var i = 0; i < names.length; i++) {
+//  	if (names[i].length===5) {
+//  		console.log(names[i]);
+//  	}
+//  }
+
+///
+
+//var favFoods = [];
+	// 	var length = prompt("Enter the desired length of the array!");
+	
+
+	// 	for(var i = 0; i < length; i++) {
+	// 	   favFoods[i] = prompt("Enter your element:" + (i+1));	   
+	// 	}
+	// 	favFoods.sort();
+	// 	console.log(favFoods);
+
+	//
+
+	// function cards(number,type){
+		// 	var number = [1,2,3,4,5,6,7,8,9,10,'J','Q','K'];
+		// 	var type = ['srce','list','detelina','baklava'];
+		// 	var cards = [];
+
+		// 	for (var i = 0; i < number.length; i++) {
+		// 		for (var j = 0; j < type.length; j++) {
+					
+		// 			console.log(number[i] + ' - ' + type[j]);
+		// 			cards.push(number[i] + ' - ' + type[j]);
+		// 		}
+		// 	}
+		// 	return cards;   // Delot okolku Return
+		// }
+		// cards();
+
+		//
+
+
+		// var random = {
+		// 	k1:"value1",
+		// 	element:"value2",
+		// 	k3:["value3.0","value3.1","value3.2","value3.3"],
+		// 	k4:"value4",
+		// 	k5:"value5",
+		// 	k6:"value6",
+		// 	vtorElement:["value7.0","value7.1","value7.2","value7.3","value7.4","value7.5", "value7.6"]
+		// }
+
+		// var elementArr = random.element[0] + ' and ' + random.element[3];
+		// var vtorElementArr = random.vtorElement[0] + ' and ' + random.vtorElement[6];
+		// return 'First and last element are ' + elementArr + ' plus ' + vtorElementArr;
+
+
+
