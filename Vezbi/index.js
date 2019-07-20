98580a2178994dd6c5c8450878734ae8abea4707
NikiDomaPc <nikolaj.grunoski@gmail.com>
2019-07-20 16:29:15 +0200

    1

---
+++ Vezbi/index.js
@@ -0,0 +1,18 @@
+function reverseStr (str){
+	var splitString = str.split("");
+	var reverseString = splitString.reverse('');
+	var joinSting = reverseString.join ('');
+
+	return joinSting;
+}
+
+// // reverseStr('Hello World');
+
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+reverseStr("hello");
+console.log(reverseStr);
+
+
+console.log('fuck off js')
